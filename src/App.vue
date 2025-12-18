82a92bd3d779e14b82ab850884c9fe4efc31578a
Ilya Rubnich <ilya@rubni.ch>
2025-12-17 21:52:28 -0500

    figuring out vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import EarthCam from './components/EarthCam.vue'
+import type { Project } from './types'
+
+const projects: Project[] = [
+  {
+    name: 'Tonnelle Ave.',
+    desc: 'short desccc',
+    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88KKPTYvGpuC9sn7UZHiL1L4.tJ90CoLmq7TzrY396Yd88KndwTghhAJ91QbmhhD8hKQ'
+  }
+]
 </script>
 
 <template>
   <main>
     <HelloWorld msg="You did it!" />
+    <div v-for="project in projects" :key="project.name">
+      <EarthCam :project />
+    </div>
   </main>
 </template>
 
---
+++ src/components/EarthCam.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Project } from '../types'
+defineProps<{
+  project: Project
+}>()
+</script>
+
+<template>
+  <div class="earthcam">
+    <h1>{{ project.name }}</h1>
+    <p>{{ project.desc }}</p>
+    <iframe :src="project.earthcam" />
+  </div>
+</template>
+
+<style scoped>
+  iframe {
+    width: 800px;
+    height: 600px;
+  }
+</style>
