a17571f9ab34f9dcd89be37d17f9a54432238b6f
Ilya Rubnich <ilya@rubni.ch>
2025-12-17 22:07:48 -0500

    might as well start with cams

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import EarthCam from './components/EarthCam.vue'
-import type { Project } from './types'
-
-const projects: Project[] = [
-  {
-    name: 'Tonnelle Ave.',
-    desc: 'short desccc',
-    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88KKPTYvGpuC9sn7UZHiL1L4.tJ90CoLmq7TzrY396Yd88KndwTghhAJ91QbmhhD8hKQ'
-  }
-]
+import { projects } from './assets/data'
 </script>
 
 <template>
   <main>
-    <HelloWorld msg="You did it!" />
     <div v-for="project in projects" :key="project.name">
       <EarthCam :project />
     </div>
---
+++ src/assets/data.ts
@@ -0,0 +1,29 @@
+import type { Project } from '../types'
+
+export const projects: Project[] = [
+  {
+    name: 'Tonnelle Ave.',
+    desc: 'tonnelle over tunnel',
+    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88KKPTYvGpuC9sn7UZHiL1L4.tJ90CoLmq7TzrY396Yd88KndwTghhAJ91QbmhhD8hKQ'
+  },
+  {
+    name: 'HYCC3',
+    desc: 'tunnel box',
+    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88P0bHGwxARMYJSW9OX-yWIs!.tJ90CoLmq7TzrY396Yd88Ghihgs7KQYXaVZwYJQ3kKA!'
+  },
+  {
+    name: 'Hudson River Ground Stabilization',
+    desc: 'pump dirt into river',
+    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88FEc2KIPXPpEyk01ZY6DV6k.tJ90CoLmq7TzrY396Yd88MxvGIEVbP8q434ixctMxNg'
+  },
+  {
+    name: 'Palisades Tunnel (Hudson County Access Shaft)',
+    desc: 'drill thru rock',
+    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88KKPTYvGpuC9sn7UZHiL1L4.tJ90CoLmq7TzrY396Yd88KH9bh2NhJbWVpd0qGjtFqY'
+  },
+  {
+    name: 'Manhattan Tunnel',
+    desc: 'drill thru water',
+    earthcam: 'https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88AalamigCKiwQn4WPzxftJM.tJ90CoLmq7TzrY396Yd88OFHG2cZ-RCafkbJvvWEIZs'
+  },
+]
