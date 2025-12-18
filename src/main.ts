fbfde6d04de10eb74b041c7096346a9fa085b6a2
Ilya Rubnich <ilya@rubni.ch>
2025-12-18 00:56:34 -0500

    datadog

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
-import { datadogRum } from '@datadog/browser-rum'
-
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-datadogRum.init({
-    applicationId: 'dbadf460-c909-44e2-ae00-f89f8395ee1a',
-    clientToken: 'pub10c57c6446818cbb642286a0e639eb93',
-    site: 'datadoghq.com',
-    service: 'gateway-tracker',
-    env: 'prod',
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
-    trackBfcacheViews: true,
-    defaultPrivacyLevel: 'mask-user-input',
-});
 
 createApp(App).mount('#app')
