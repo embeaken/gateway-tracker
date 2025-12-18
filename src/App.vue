97f7ce035376c89fb50c6fa3bcadf755e8612d23
Ilya Rubnich <ilya@rubni.ch>
2025-12-17 21:18:36 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld msg="You did it!" />
+  </main>
+</template>
+
+<style scoped>
+</style>
